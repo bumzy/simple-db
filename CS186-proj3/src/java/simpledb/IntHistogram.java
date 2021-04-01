cacde44af49953cea7b6622e97dd86b39bddc489
bumzy <bumzycm@gmail.com>
2021-04-02 00:13:35 +0800

    update

--- CS186-proj3/src/java/simpledb/IntHistogram.java
+++ CS186-proj3/src/java/simpledb/IntHistogram.java
@@ -4,24 +4,30 @@ package simpledb;
  */
 public class IntHistogram {
 
+    private int buckets = 0;
+    private int min = Integer.MAX_VALUE;
+    private int max = Integer.MIN_VALUE;
+
     /**
      * Create a new IntHistogram.
-     * 
+     *
      * This IntHistogram should maintain a histogram of integer values that it receives.
      * It should split the histogram into "buckets" buckets.
-     * 
+     *
      * The values that are being histogrammed will be provided one-at-a-time through the "addValue()" function.
-     * 
+     *
      * Your implementation should use space and have execution time that are both
-     * constant with respect to the number of values being histogrammed.  For example, you shouldn't 
+     * constant with respect to the number of values being histogrammed.  For example, you shouldn't
      * simply store every value that you see in a sorted list.
-     * 
+     *
      * @param buckets The number of buckets to split the input value into.
      * @param min The minimum integer value that will ever be passed to this class for histogramming
      * @param max The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-    	// some code goes here
+    	this.buckets = buckets;
+        this.min = min;
+        this.max = max;
     }
 
     /**
@@ -34,10 +40,10 @@ public class IntHistogram {
 
     /**
      * Estimate the selectivity of a particular predicate and operand on this table.
-     * 
-     * For example, if "op" is "GREATER_THAN" and "v" is 5, 
+     *
+     * For example, if "op" is "GREATER_THAN" and "v" is 5,
      * return your estimate of the fraction of elements that are greater than 5.
-     * 
+     *
      * @param op Operator
      * @param v Value
      * @return Predicted selectivity of this particular operator and value
@@ -47,11 +53,11 @@ public class IntHistogram {
     	// some code goes here
         return -1.0;
     }
-    
+
     /**
      * @return
      *     the average selectivity of this histogram.
-     *     
+     *
      *     This is not an indispensable method to implement the basic
      *     join optimization. It may be needed if you want to
      *     implement a more efficient optimization
@@ -61,7 +67,7 @@ public class IntHistogram {
         // some code goes here
         return 1.0;
     }
-    
+
     /**
      * @return A string describing this histogram, for debugging purposes
      */
